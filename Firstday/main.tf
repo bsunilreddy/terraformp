819473a68282b74b27319a61531af0123b902d38
bsunilreddy <104701437+bsunilreddy@users.noreply.github.com>
2023-09-30 16:25:48 -0400

    EC2

--- Firstday/main.tf
+++ Firstday/main.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "firsttreeaform" {
     ami = "ami-0bb4c991fa89d4b9b"
     instance_type = "t2.micro"
     count = "3"  
-}
-tags = {
+   tags = {
     "Name" = "MyInstanceName"  # Replace with the desired name for your instance
     "Environment" = "Production"  # You can add additional tags as needed
   }
+}
